b770c52b79e0ee3cf9dbb1deef1c90a324ebd452
Vadim Polyakov <vadim@preparedmind.net>
2018-02-09 11:58:27 -0500

    move computers

--- src/Text.Game.Console/Program.cs
+++ src/Text.Game.Console/Program.cs
@@ -11,25 +11,28 @@ namespace Text.Game.Console
             
             System.Console.WriteLine("What is your name?");
             GameState.Player.Name =  System.Console.ReadLine();
+            Welcome();
 
             bool keepPlaying = true;
 
             while(keepPlaying) {
-                System.Console.WriteLine(  
-                    String.Format("Welcome {0}.  You are in the {1}. {2}",
-                    GameState.Player.Name, 
-                    GameState.CurrentPlace.Name, 
-                    GameState.CurrentPlace.Description ) );
-                DescribeGameObjects(GameState.CurrentPlace.GameObjects);                
-
             
                 string prompt = String.Format("{0}, what is your command?", GameState.Player.Name);
                 System.Console.WriteLine(prompt);
                 string command = System.Console.ReadLine();
-                
-                keepPlaying = command.ToLower() != "quit";
 
-                GameState.Navigate(command);
+                switch(command.ToLower()){
+                    case "quit":
+                        keepPlaying = false;
+                        break;
+                    case "inventory":
+                        ShowInventory(GameState.Player.GameObjects);
+                        break;
+                    default:
+                        GameState.Navigate(command);
+                        Welcome();
+                        break;
+                }
 
                 //if(command.ToLower()== "go left" && GameState.CurrentPlace == bedroom) {
                 //    GameState.CurrentPlace = kitchen;
@@ -39,6 +42,32 @@ namespace Text.Game.Console
             System.Console.WriteLine("Goodbye!");
 
             
+        }
+
+        private static void Welcome(){
+
+            System.Console.WriteLine(  
+                    String.Format("Welcome {0}.  You are in the {1}. {2}",
+                    GameState.Player.Name, 
+                    GameState.CurrentPlace.Name, 
+                    GameState.CurrentPlace.Description ) );
+                DescribeGameObjects(GameState.CurrentPlace.GameObjects);
+
+        }
+
+        private static void ShowInventory(List<GameObject> gameObjects) {
+            if (GameState.Player.GameObjects.Count == 0) {
+                System.Console.WriteLine("You have nothing.");
+                return;
+            } 
+            
+            System.Console.Write("You have: ");
+            foreach(GameObject o in GameState.Player.GameObjects) {
+                System.Console.Write(o.Name + ", ");
+                if(o.GameObjects.Count > 0 ){
+                    ShowInventory(o.GameObjects);
+                }
+            }
         }
 
         private static void DescribeGameObjects(List<GameObject> gameObjects) {
--- src/Text.Game.Objects/Player.cs
+++ src/Text.Game.Objects/Player.cs
@@ -1,9 +1,15 @@
-using System;
+using System.Collections.Generic;
 
 namespace Text.Game.Objects
 {
     public class Player
     {
+
+        public Player() {
+            this.GameObjects = new List<GameObject>();
+        }
         public string Name {get;set;}
+
+        public  List<GameObject> GameObjects { set; get;}
     }
 }
